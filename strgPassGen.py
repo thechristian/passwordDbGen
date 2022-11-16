9dc0d81f30150d3bcfff1c3bb5c4a29f8017a5a3
Christian Otoo <christianotoo@gmail.com>
2022-11-16 09:55:49 +0000

    put content into a function to make code simpler

--- strgPassGen.py
+++ strgPassGen.py
@@ -1,49 +1,51 @@
 # This was written by christian otoo for your free use.
+# You can modify for your use case.
 
 import random
 import string
 
-print('Hello, Welcome to Password generator db!')
-
-# user input for password length
-passLength = int(input('\nEnter the length of password >> '))
-
-# user input for the quantity of passwords
-quantity = int(input('\nEnter the quantity of passwords to be generated >> '))
-
-# a list to hold each generated passwords
-passList = []
-
-# specifying password character combinations
-lowerC = string.ascii_lowercase
-upperC = string.ascii_uppercase
-numbers = string.digits
-symbols = string.punctuation
-
-# creating long characters of password combinations
-allChars = lowerC + upperC + numbers + symbols
 
 
-# create password according to quantity and lenth specified
-counter = 1
-while counter <= quantity:
-    temp = random.sample(allChars,passLength)
-    password = "".join(temp)
-    # print (password)
-    passList.append(password)
-    counter += 1
+def passGen(passLength, quantity):
 
-# create password file name using password lenght and quantity
-fileName = str(passLength)+"of"+str(quantity)+".txt"
+    # passLength = the length of password
+    # quantity = the quantity of passwords to be generated
+   
+    passList = []    # a list to hold each generated passwords
 
-# saving all generated passwords into the file
-for i in passList:
-    passFile = open(fileName, "a") 
-    passFile.write("{}\n".format(i))
-    passFile.close()
+    # specifying password character combinations
+    lowerC = string.ascii_lowercase
+    upperC = string.ascii_uppercase
+    numbers = string.digits
+    symbols = string.punctuation
+
+    # creating long characters of password combinations
+    allChars = lowerC + upperC + numbers + symbols
 
 
-# Display process complete notice
-print("=============================")
+    # create password according to quantity and lenth specified
+    counter = 1
+    while counter <= quantity:
+        temp = random.sample(allChars,passLength)
+        password = "".join(temp)
+        # print (password)
+        passList.append(password)
+        counter += 1
 
-print("All passwords generated in your working directory.")
+    # create password file name using password lenght and quantity
+    fileName = str(quantity)+"of"+str(passLength)+".txt"
+
+    # saving all generated passwords into the file
+    for i in passList:
+        passFile = open(fileName, "a") 
+        passFile.write("{}\n".format(i))
+        passFile.close()
+
+
+    # Display process complete notice
+    print("                                                  ")
+    print("==================================================")
+    print("                                                  ")
+    print("All passwords generated in your working directory.")
+    print("                                                  ")
+    print("==================================================")
